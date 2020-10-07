8446c3405a889b6d91154571fb9d2366569beedb
lambco <kanievalism@gmail.com>
2020-10-08 08:32:37 +0900

    chapter2完了

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-    region = "ap-northeast-1"
+  region = "ap-northeast-1"
 }
 
 resource "aws_vpc" "vpc_zone" {
@@ -10,4 +10,41 @@ resource "aws_vpc" "vpc_zone" {
   tags = {
     Name = "VPC領域"
   }
+}
+
+resource "aws_internet_gateway" "gateway" {
+  vpc_id = aws_vpc.vpc_zone.id
+}
+
+resource "aws_route_table" "route_table" {
+  vpc_id = aws_vpc.vpc_zone.id
+}
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.vpc_zone.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "ap-northeast-1a"
+
+  tags = {
+    Name = "パブリックサブネット"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.vpc_zone.id
+  tags = {
+    Name = "パブリックルートテーブル"
+  }
+}
+
+resource "aws_route" "public" {
+  route_table_id         = aws_route_table.public.id
+  gateway_id             = aws_internet_gateway.gateway.id
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
 }
